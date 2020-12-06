9b5c9641a867562dc014fffe2fe62a7224ac7f2c
snigdho48 <www.atik48@gmail.com>
2020-12-06 19:15:33 +0600

    admin panel initial

---
+++ dashboard/urls.py
@@ -0,0 +1,42 @@
+"""adminpanel URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path ,include
+from django.conf.urls import url
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^tables/',views.tables,name="tables"),
+    url(r'^charts/',views.charts,name="charts"),
+    url(r'^cards/',views.cards,name="cards"),
+    url(r'^500/',views.e500,name="e500"),
+    url(r'^404/',views.e404,name="e404"),
+    url(r'^401/',views.e401,name="e401"),
+    url(r'^login/',views.login,name="login"),
+    url(r'^register/',views.register,name="register"),
+    url(r'^password/',views.password,name="password"),
+    url(r'^light/',views.layout,name="layout"),
+    url(r'^',views.base,name="base"),
+
+
+
+
+
+]
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ dashboard/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+# Create your views here.
+def layout(request):
+    return render(request,'layout-light.html')
+
+def base(request):
+    return render(request,'base.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def password(request):
+    return render(request,'password.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def tables(request):
+    return render(request,'tables.html')
+
+def charts(request):
+    return render(request,'charts.html')
+
+def cards(request):
+    return render(request,'cards.html')
+
+def e500(request):
+    return render(request,'500.html')
+
+def e401(request):
+    return render(request,'401.html')
+
+def e404(request):
+    return render(request,'404.html')
