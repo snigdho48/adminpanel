5b05f907807a12ade554ce135f73656a6ee899fe
snigdho48 <www.atik48@gmail.com>
2020-12-08 18:18:06 +0600

    fix some issu

--- adminpanel/settings.py
+++ adminpanel/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'furioussnigdho@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'acvfeejsuenepzsb'
